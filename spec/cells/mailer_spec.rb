48772d24a1fa8e715f1f7df57c6dc9b09bac2009
Timo Schilling <timo@schilling.io>
2015-11-05 20:50:14 +0100

    remove useless spec

--- spec/cells/mailer_spec.rb
+++ spec/cells/mailer_spec.rb
@@ -4,8 +4,4 @@ describe Cells::Mailer do
   it 'has a version number' do
     expect(Cells::Mailer::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
