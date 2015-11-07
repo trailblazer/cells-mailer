e6b27e9b10e922350bc646b209149808b7c2e3d2
Timo Schilling <timo@schilling.io>
2015-11-07 22:35:56 +0100

    spec subject(:mail)

--- spec/cell/mailer_spec.rb
+++ spec/cell/mailer_spec.rb
@@ -10,13 +10,13 @@ end
 
 RSpec.describe Cell::Mailer do
   subject(:cell) { MailerCell.(nil) }
+  subject(:mail) { Mail::TestMailer.deliveries.first }
 
   let(:options) { { from: "foo@example.org", to: "bar@example.org", subject: "example" } }
 
   it "delivers a Mail" do
     cell.deliver(options)
     expect(Mail::TestMailer.deliveries.count).to eq 1
-    mail = Mail::TestMailer.deliveries.first
     expect(mail.from).to eq ["foo@example.org"]
     expect(mail.to).to eq ["bar@example.org"]
     expect(mail.subject).to eq "example"
