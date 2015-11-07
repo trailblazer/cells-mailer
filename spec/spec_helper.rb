7d4767460fecb8965897822475be870e1785f54f
Timo Schilling <timo@schilling.io>
2015-11-07 23:20:14 +0100

    rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,14 @@ RSpec.configure do |config|
   config.after(:each) do
     Mail::TestMailer.deliveries.clear
   end
+
+  # These three settings work together to allow you to limit a spec run to
+  # individual examples or groups you care about by tagging them with
+  # `:focus` or `:skip` metadata. When nothing is tagged with `:focus`,
+  # all examples get run.
+  config.filter_run_including :focus
+  config.filter_run_excluding :skip
+  config.run_all_when_everything_filtered = true
+
+  config.expose_dsl_globally = false
 end
